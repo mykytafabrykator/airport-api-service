aa0e1eaff5f89754ecd8783b825fcbacfdeb467e
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-24 16:32:40 +0200

    Add media settings and install Pillow for image support

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -171,7 +175,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=100),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "ROTATE_REFRESH_TOKENS": False,
 }
